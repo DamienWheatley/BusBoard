f0f9dd19b7f5a1048adbfc4ac2042bc5dd6b10ed
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2018-11-07 11:54:46 +0000

    Starting Part 4

--- App.js
+++ App.js
@@ -1,51 +1,8 @@
-const readline = require('readline-sync');
-const importParse = require('./Parser.js');
-const importClass = require('./BusArrival.js');
-const moment = require('moment');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//Filters information for user inputted Bus Code
-function filteringData(array) {
-    let retrievedData = [];
-    let numberOfBuses = Math.min(array.length,5);
-    for(i = 0; i <= numberOfBuses - 1; i++) {
-        retrievedData.push(new importClass.BusArrival(array[i].vehicleId,array[i].timeToStation,array[i].lineName,array[i].towards))
-    }
-    if (retrievedData.length > 0) {
-        retrievedData.sort(sortData);
-        retrievedData.forEach(function(element) {
-        console.log("Arriving " + moment().add(element.timeToStation,"seconds").fromNow() + " is bus " + element.lineName + " towards " + element.towards + " with vehicle id " + element.vehicleId )
-        })
-    } else {
-        console.log(`There was no information for that bus code :'(`);
-    }
-}
+app.get('/departureBoards', (req, res) => res.send('Hello World!'));
+//http://localhost:3000/departureBoards
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//Provides the filtering criteria for the Parser and returns the filtered information into an array.
-function runProgram() {
-    var busCode = userInput();
-    var url = createsArrivalsURL(busCode);
-    importParse.parseURL(filteringData,url);
-}
-
-function createsArrivalsURL(input) {
-    const url = "https://api.tfl.gov.uk/StopPoint/" + input + "/Arrivals";
-    return url;
-}
-
-function userInput() {
-    console.log("Please enter the bus code:");
-    var userInput = readline.prompt();
-    return userInput;
-}
-
-function sortData(bus1,bus2) {
-    if (bus1.timeToStation < bus2.timeToStation) {
-        return -1;
-    } else if (bus1.timeToStation > bus2.timeToStation) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-runProgram();
